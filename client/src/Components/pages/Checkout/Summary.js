eecb55a2010bdd532112472a686078761048cc79
Trevor Johnson <johnsontrevor55@gmail.com>
2018-11-26 00:49:43 -0700

    Refactored code to determine error message

--- client/src/Components/pages/Checkout/Summary.js
+++ client/src/Components/pages/Checkout/Summary.js
@@ -32,7 +32,6 @@ class Summary extends React.Component {
 			loading: false,
 			errorMsg: null
 		};
-		console.log(this.props.location.state);
 		this.toggleCollapse = this.toggleCollapse.bind(this);
 		this.eventDetails = this.props.location.state;
 		this.detailsFirst = Object.keys(this.eventDetails.eventProps).slice(0, 6);
@@ -78,7 +77,6 @@ class Summary extends React.Component {
 				this.toggle(true, result.data.estimateId);
 			})
 			.catch(error => {
-				console.log(error.message);
 				const err =
 					error.message && error.message.includes('timeout')
 						? 'Connection timed out'
